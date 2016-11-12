a3db52e8e5956e2b3642cfba70731fc4fd7fc3fa
Sara Pearce <sarapearce3.14@gmail.com>
2016-11-12 15:20:03 +0000

    prettify python

--- comesg/items.py
+++ comesg/items.py
@@ -7,11 +7,10 @@ from scrapy.item import Item, Field
 
 class ComesgItem(Item):
     # define the fields for your item here like:
-    name = Field()
-    address = Field()
-    contact = Field()
-    hours = Field()
-    website = Field()
-    photo = Field()
-    desc = Field()
-    video = Field()
+    Date = Field()
+    Title = Field()
+    RaceOrTopic = Field()
+    Poll = Field()
+    Results = Field()
+    Spread = Field()
+    
--- comesg/spiders/get-data.py
+++ comesg/spiders/get-data.py
@@ -6,98 +6,96 @@ from scrapy import log
 from comesg.items import ComesgItem
 
 class AttractionSpider(CrawlSpider):
-	name = "get-data"
-	allowed_domains = ["http://www.realclearpolitics.com"]
-	start_urls = [
-		"http://www.realclearpolitics.com/epolls/latest_polls/elections/"
-	]
-	rules = ()
+    name = "get-data"
+    allowed_domains = ["http://www.realclearpolitics.com"]
+    start_urls = [
+        "http://www.realclearpolitics.com/epolls/latest_polls/elections/"
+    ]
+    rules = ()
 
-	def __init__(self, name=None, **kwargs):
-		super(AttractionSpider, self).__init__(name, **kwargs)
-		self.items_buffer = {}
-		self.base_url = "http://www.realclearpolitics.com"
-		from scrapy.conf import settings
-		settings.overrides['DOWNLOAD_TIMEOUT'] = 360
+#    intact from orig author
+    def __init__(self, name=None, **kwargs):
+        super(AttractionSpider, self).__init__(name, **kwargs)
+        self.items_buffer = {}
+        self.base_url = "http://www.realclearpolitics.com"
+        from scrapy.conf import settings
+            settings.overrides['DOWNLOAD_TIMEOUT'] = 360
 
-	def parse(self, response):
-		print "Start scrapping election data...."
-		try:
-			hxs = HtmlXPathSelector(response)
-			links = hxs.select("//*[@id='table-1']//a[@style='color:black']/@href")
-			
-			if not links:
-				return
-				log.msg("No Data to scrape")
+#    intact from orig author
+    def parse(self, response):
+        print "Start scrapping election data...."
+        try:
+            hxs = HtmlXPathSelector(response)
+#            TODO: this linking might not be necessary. Delete if not used
+            links = hxs.select("//*[@id='table-1']//a[@style='color:black']/@href")
+            if not links:
+                return
+                log.msg("No Data to scrape")
 
-			for link in links:
-				v_url = ''.join( link.extract() )
-								
-				if not v_url:
-					continue
-				else:
-					_url = self.base_url + v_url
-					yield Request( url= _url, callback=self.parse_details )
-		except Exception as e:
-			log.msg("Parsing failed for URL {%s}"%format(response.request.url))
-			raise 
+            for link in links:
+                v_url = ''.join( link.extract() )
 
-	def parse_details(self, response):
-		print "Collecting data points...."
-		try:
-			hxs = HtmlXPathSelector(response)
-			l_venue = ComesgItem()
+            if not v_url:
+                    continue
+                else:
+                    _url = self.base_url + v_url
+                    yield Request( url= _url, callback=self.parse_details )
+        except Exception as e:
+            log.msg("Parsing failed for URL {%s}"%format(response.request.url))
+            raise 
+
+#    unique to the website
+    def parse_details(self, response):
+        print "Collecting data points...."
+        try:
+            hxs = HtmlXPathSelector(response)
+            poll_data = ComesgItem()
 #            update this part to traverse down ML nodes specific to a given site
 
 #            for RCP, this traverses to the innerHTML of the 1st tables cells
-			v_name = hxs.select("/html/body/div[@id='container']/div[@id='alpha-container']/div[@id='alpha']/div[@id='table-1']/table/tbody/tr/td/a/text()").extract()
-			if not v_name:
+            v_name = hxs.select("/html/body/div[@id='container']/div[@id='alpha-container']/div[@id='alpha']/div[@id='table-1']/table/tbody/tr/td/a/text()").extract()
+            if not v_name:
 #                might get an array of tables returned, specify index to try again
-				v_name = hxs.select("/html/body/div[@id='container']/div[@id='alpha-container']/div[@id='alpha']/div[@id='table-1']/table[0]/tbody/tr/td/a/text()").extract()
-			
-			l_venue["name"] = v_name[0].strip()
-			
-#            assign base var so we do not have to write long strings for traversing to base nodes
-			base = hxs.select("//*[@id='table-1']/div[18]")
-			if base.extract()[0].strip() == "<div style=\"clear:both\"></div>":
-				base = hxs.select("//*[@id='content']/div[8]")
-			elif base.extract()[0].strip() == "<div style=\"padding-top:10px;margin-top:10px;border-top:1px dotted #DDD;\">\n  You must be logged in to add a tip\n  </div>":
-				base = hxs.select("//*[@id='content']/div[6]")
+                v_name = hxs.select("/html/body/div[@id='container']/div[@id='alpha-container']/div[@id='alpha']/div[@id='table-1']/table[0]/tbody/tr/td/a/text()").extract()
+#            On RealClearPolitics, all tables have a header of the poll, grab that name
+            title = hxs.select("/html/body/div[@id='container']/div[@id='alpha-container']/div[@id='alpha']/div[@id='page_title']/div/text()").extract()
+    
+#            TODO: this is now a test item. fix whitespace error after I figure out how to see whitespace in Brackets
+#            not title:
+#                title="no title found"
+            poll_data["Title"] = title[0].strip()
 
-			x_datas = base.select("div[1]/b").extract()
-			v_datas = base.select("div[1]/text()").extract()
-			i_d = 0;
-			if x_datas:
-				for x_data in x_datas:
-					print "data is:" + x_data.strip()
-					if x_data.strip() == "<b>Address:</b>":
-					 	l_venue["address"] = v_datas[i_d].strip()
-					if x_data.strip() == "<b>Contact:</b>":
-					 	l_venue["contact"] = v_datas[i_d].strip()
-					if x_data.strip() == "<b>Operating Hours:</b>":
-					 	l_venue["hours"] = v_datas[i_d].strip()
-					if x_data.strip() == "<b>Website:</b>":
-					 	l_venue["website"] = (base.select("div[1]/a/@href").extract())[0].strip()
+#            assign base var so we do not have to write long strings for traversing to deep nodes
+            base = hxs.select("//*[@id='table-1']")
+            tables = base.select("table").extract()
 
-					i_d += 1
-				
-			v_photo = base.select("img/@src").extract()
-			if v_photo:
-				l_venue["photo"] = v_photo[0].strip()
+            i_d = 0
+            if tables:
+                for table in tables:
+                    print "processing table  " + i_d
+                    tds = table.select("tbody/tr/td")
+#                        fill in the poll_data object with cell data
+                    print tds
+                        if tds:
+                            for td in tds:
+                                td_class = td.extract()
+                                if "date" in td_class:
+                                    poll_data["Date"] = td.text()
+                                if "lp-race" in td_class:
+                                    poll_data["RaceOrTopic"] = td.text()
+                                if "lp-poll" in td_class:
+                                    poll_data["Poll"] = td.text()
+                                if "lp-results" in td_class:
+                                    poll_data["Results"] = td.text()
+                                if "lp-spread" in td_class:
+                                    poll_data["Spread"] = td.text()
+                    i_d += 1
 
-			v_desc = base.select("div[3]/text()").extract()
-			if v_desc:
-				desc = ""
-				for dsc in v_desc:
-					desc += dsc
-				l_venue["desc"] = desc.strip()
+            yield poll_data
+            print poll_data
+        except Exception as e:
+            log.msg("Parsing failed for URL {%s}"%format(response.request.url))
+            raise
 
-			v_video = hxs.select("//*[@id='content']/iframe/@src").extract()
-			if v_video:
-				l_venue["video"] = v_video[0].strip()
-
-
-			yield l_venue
-		except Exception as e:
-			log.msg("Parsing failed for URL {%s}"%format(response.request.url))
-			raise 
+#            run this line in the root folder of this directory to run the script
+#            scrapy crawl get-data -t csv -o rcp-data-1.csv
